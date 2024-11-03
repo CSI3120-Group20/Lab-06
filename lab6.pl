e3b6c1c25ffa6383ec08907a1723acb840f52a94
jordanxlau <jordanxlau@yahoo.ca>
2024-11-02 21:11:49 -0400

    bugfixes to Task A Question 1

--- lab6.pl
+++ lab6.pl
@@ -1,14 +1,16 @@
 %  Task A
 
-% Question 1 (Jordan had already implemented this in another course)
-factorial(N, R) :-
+% Question 1
+% Code based on a factorial program implemented in another course
+factorial(N, _, R) :-
     N>0,
     N1 is N-1,
-    factorial(N1, R1),
-    write(R1), write(' '),
-    /*go down to R1=1, then go back up again multiplying each R by N*/ R is R1*N.
+    factorial(N1, _, R1),
+    /*go down to R1=1, then go back up again multiplying each R by N*/
+    R is R1*N.
 
-factorial(0,1).
+factorial(0, _, 1).
+factorial(N, _, R) :- N < 0, R = 'Error: Input must be a non-negative integer.'. % Note that 'is' evaluates arithmetic expressions, '=' sets a variable to other datatypes
 
 % Question 2
 
