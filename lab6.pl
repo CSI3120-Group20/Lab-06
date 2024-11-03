07c3f29b40bfa6545635d707e4c5d98d077417fc
jordanxlau <jordanxlau@yahoo.ca>
2024-11-02 21:22:25 -0400

    Task A Question 2

--- lab6.pl
+++ lab6.pl
@@ -13,6 +13,22 @@ factorial(0, _, 1).
 factorial(N, _, R) :- N < 0, R = 'Error: Input must be a non-negative integer.'. % Note that 'is' evaluates arithmetic expressions, '=' sets a variable to other datatypes
 
 % Question 2
+% Both True Case
+filter_list([H|T], [G, M], Result) :- conditions(H, G, M), filter_list(T, [G, M], NewResult), Result = [H|NewResult].
+
+% One False Case
+filter_list([H|T], [G, M], Result) :- not(conditions(H, G, M)), filter_list(T, [G, M], Result).
+
+% Base Case True
+filter_list([H], [G, M], Result) :- conditions(H, G, M), Result = [H].
+
+% Base Case False
+filter_list([H], [G, M], Result) :- not(conditions(H, G, M)), Result = [].
+
+% Supplementary predicates
+conditions(H, G, M) :- greater_than(H, G), multiple_of(H, M).
+greater_than(A, B) :- A>B.
+multiple_of(A, B) :- Remainder is A mod B, Remainder = 0.
 
 % Question 3
 
